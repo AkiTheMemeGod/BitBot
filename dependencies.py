4eafde949c3278a006ddb3633ee4ae6006fa9283
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-04-04 19:32:35 +0530

    Add moderators management and enhance bot responses with new commands

--- dependencies.py
+++ dependencies.py
@@ -1,14 +1,64 @@
+import re
+from better_profanity import profanity
+profanity.load_censor_words()
+
+
 def get_prefix(bot, message):
     with open("prefix.txt", 'r') as file:
         prefix = file.read()
     return prefix
 
 
-def get_pre():
-    with open("prefix.txt", 'r') as file:
-        prefix = file.read()
-    return prefix
+def get(path, islist: bool = False):
+    if islist:
+        with open(path, 'r') as file:
+            li = file.readlines()
+        return li
+    else:
+        with open(path, 'r') as file:
+            prefix = file.read()
+        return prefix
 
-def put_prefix(arg, filepath="prefix.txt"):
-    with open(filepath, 'w') as file:
-        file.write(arg)
+def put(content, path, islist:bool = False):
+    if islist:
+        li = get("Moderators.txt", True)
+        with open(path, 'w') as file:
+            print(li)
+            li.append(content+"\n")
+            print(li)
+            file.writelines(li)
+            print(li)
+            file.close()
+        return None
+    with open(path, 'w') as file:
+        file.write(content)
+        return None
+
+def contains_prohibited_content(message):
+    """Check if a message contains banned words or regex matches."""
+    message_lower = message.lower()
+
+    if profanity.contains_profanity(message_lower):
+        return True
+
+    for pattern in custom_banned_words:
+        if re.search(pattern, message_lower):
+            return True
+
+    return False
+
+custom_banned_words = [
+    r"\b[b8][a@4][dD]w[o0]rd\b",
+    r"\b[o0]ffensive[s$]*\b",
+    r"\b[nN][s$5][fF][wW]\b",
+    r"\b[fF][uU][cC][kK]\b",
+    r"\b[s$][hH][i1!][tT]+\b",
+    r"\b[dD][i1!][cC][kK]\b",
+    r"\b[aA@][sS5][sS5]\b",
+    r"\b[cC][uU][nN][tT]\b",
+    r"\b[nN][i1!][gG][gG][eE3][rR]\b",
+    r"\b[hH][oO0][eE3]\b",
+    r"\b[bB][i1!][tT][cC][hH]\b",
+    r"\b[fF][aA@][gG]{1,2}\b",
+    r"\b[mM][oO0][tT][hH][eE3]r*[fF][uU][cC][kK][eE3]r*\b",
+]
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
-
 import tokens
 from dependencies import *
+import random
+import bot_responses as br
+import requests
+from bs4 import BeautifulSoup
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,8 +26,6 @@ async def send_message(ctx, response, is_private):
 async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
-
-
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now running!')
@@ -40,6 +41,83 @@ async def on_ready():
                                    "https://tenor.com/view/getting-online-getting-online-gif-27546602")
     else:
         print("Could not find the specified channel for startup message.")
+
+#FUN PARTS
+@bot.command(name='roast')
+async def roast(ctx, person: discord.User = ""):
+    if person != "":
+        response = f"{person.mention} {random.choice(br.roasts)}"
+        await ctx.send(response)
+
+    else:
+        response = f"{ctx.author.mention} {random.choice(br.roasts_for_missing_argument)}"
+        await ctx.send(response)
+
+@bot.command()
+async def meme(ctx):
+    url = "https://www.reddit.com/r/memes/hot.json?limit=50"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            memes = response.json()["data"]["children"]
+            meme = random.choice(memes)["data"]
+            embed = discord.Embed(title=meme["title"], url=f"https://reddit.com{meme['permalink']}")
+            embed.set_image(url=meme["url"])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Couldn't fetch memes, try again later!")
+    except Exception as e:
+        await ctx.send("Error fetching memes!")
+        print(f"Error: {e}")
+
+@bot.command()
+async def rolldice(ctx):
+    roll = random.randint(1,6)
+    await ctx.send(roll)
+
+@bot.event
+async def on_message(message):
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+    event = f'{username} said: "{user_message}" ({channel})'
+    with open("log.log", "a") as log_file:
+        log_file.write(event + "\n")
+    print(event)
+
+
+    if message.author == bot.user:
+        return
+
+    if contains_prohibited_content(message.content):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}, 🚫 please follow the server rules!")
+
+    if f"{get("prefix.txt")}change prefix to" in user_message:
+        put(user_message[18:19], "prefix.txt")
+        response = f"{message.author.mention} the prefix has been changed to `{get('prefix.txt')}`"
+        await message.channel.send(response)
+
+    if "prefix?" == user_message:
+        response = f"`{get("prefix.txt")}` is the current prefix for the bot-->{bot.user}"
+        await message.channel.send(response)
+
+    await bot.process_commands(message)
+
+
+@bot.command(name='whitelist')
+async def whitelist(ctx, person: discord.User = ""):
+    if str(ctx.author)+"\n" in get("Moderators.txt", True):
+        print("in command")
+        put(person.name, "Moderators.txt",True)
+        await ctx.send(f"Added {person.mention} as a Moderator")
+
+
+
+
+
+
 
 def run_discord_bot():
     bot.run(tokens.token)
